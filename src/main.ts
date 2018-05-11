7016792700fe5fb3911391d284abf7ed4df7b443
Saravanan Krishnan <sakrishnan@ghx.com>
2018-05-11 13:28:24 +0530

    Added ts lint for all the files

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+    .catch((err: any) => {
+        // console.error('Error' + err);
+    });
